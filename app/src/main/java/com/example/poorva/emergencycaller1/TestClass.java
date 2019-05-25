789c247014b9dfcdff6be1a1096e762eb2a247c0
poorva-thosar <50984783+poorva-thosar@users.noreply.github.com>
2019-05-25 09:22:34 +0530

    Update TestClass.java

--- app/src/main/java/com/example/poorva/emergencycaller1/TestClass.java
+++ app/src/main/java/com/example/poorva/emergencycaller1/TestClass.java
@@ -28,21 +28,15 @@ public class TestClass extends AppCompatActivity implements AsyncResponse1 {
         textView = findViewById(R.id.id_textview1);
         Bundle bundle = getIntent().getExtras();
         String output1 = bundle.getString("message");
+        
 /////////////////////////
         emergencyButton = (Button) findViewById(R.id.callbutton);
         emergencyButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
                 Intent callIntent = new Intent(Intent.ACTION_CALL);
-                callIntent.setData(Uri.parse("tel:8830474692"));
+                callIntent.setData(Uri.parse(output));
                 if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
                     return;
                 }
                 startActivity(callIntent);
@@ -50,20 +44,17 @@ public class TestClass extends AppCompatActivity implements AsyncResponse1 {
         });
 
 /////////////////////////
-        // Toast.makeText(this,url, Toast.LENGTH_SHORT).show();
         StringBuilder sb = new StringBuilder("https://maps.googleapis.com/maps/api/place/details/json?");
         sb.append("placeid=" + output1);
-        sb.append("&fields=name,rating,formatted_phone_number&key=AIzaSyDyL9KVP-Ciro3_o72l4VbVdibDq2KloNI");
+        sb.append("&fields=name,rating,formatted_phone_number&key=YOUR API KEY");
         String url1 = sb.toString();
 
         GetPhoneNumber getPhoneNumber = new GetPhoneNumber(this);
         getPhoneNumber.delegate = this;
         getPhoneNumber.execute(this, url1);
-        //textView.setText(url1);
-    }
+     }
 
     /////////////////////////
-
     @Override
     public void processFinish(String output) {
         Toast.makeText(this, output, Toast.LENGTH_SHORT).show();
